cb5dccb24d0efc439523fca49dc508703f2e06dc
BelovEvgenii <Zhekab777@yandex.ru>
2026-03-03 23:09:57 +0300

    fix: изменить логику addComment

--- server/src/test/java/ru/practicum/shareit/item/service/ItemServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/item/service/ItemServiceImplTest.java
@@ -626,8 +626,8 @@ class ItemServiceImplTest {
         when(userRepository.findById(2L)).thenReturn(Optional.of(booker));
         when(itemRepository.findById(1L)).thenReturn(Optional.of(item));
 
-        when(bookingRepository.existsByBookerIdAndItemIdAndStatus(
-                eq(2L), eq(1L), eq(BookingStatus.APPROVED)))
+        when(bookingRepository.hasUserBookedAndApproved(
+                eq(2L), eq(1L), any(LocalDateTime.class)))
                 .thenReturn(true);
 
         when(commentRepository.save(any(Comment.class))).thenReturn(comment);
