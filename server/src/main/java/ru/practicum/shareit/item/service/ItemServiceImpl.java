ade0b0f00418a89a56ae750066e1a2c46bff75f7
BelovEvgenii <Zhekab777@yandex.ru>
2026-03-04 22:28:22 +0300

    fix: изменить логику addComment

--- server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.dto.mapper.BookingMapper;
 import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.booking.model.BookingStatus;
 import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.exception.UnauthorizedAccessException;
@@ -239,7 +238,7 @@ public class ItemServiceImpl implements ItemService {
                     .orElseThrow(() -> new NotFoundException("Вещь не найдена"));
 
             log.info("Проверка бронирования: userId={}, itemId={}", userId, itemId);
-            
+
             boolean hasApprovedBooking = bookingRepository.existsByBookerIdAndItemIdAndStatus(
                     userId, itemId);
 
